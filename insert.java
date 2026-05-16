c1efffe6e613e81650022ec98fa1fdbb0f92e58a
krishpanwar343 <krishpanwar2533@gmail.com>
2026-05-16 21:44:38 +0530

    Delete insert.java

--- insert.java
+++
@@ -1,9 +0,0 @@
-public class insert { 
-    public static void main(String[] args) { 
-        StringBuffer buffer = new StringBuffer("Hello"); 
-         
-        buffer.insert(5, " Java"); 
-         
-        System.out.println("After insert: " + buffer); 
-    } 
-} 
